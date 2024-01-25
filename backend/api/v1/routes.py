ed41682b65057264b126a78487ffd11707dc2caf
jojiku <56271473+jojiku@users.noreply.github.com>
2024-01-25 10:38:33 +0300

    Add files via upload

---
+++ backend/api/v1/routes.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+from backend.api.v1.endpoints.auth import router as auth_router
+from backend.api.v1.endpoints.billing import router as billing_router
+from backend.api.v1.endpoints.prediction import router as predicting_router
+
+routers = APIRouter()
+router_list = [auth_router, billing_router, predicting_router]
+
+for router in router_list:
+    router.tags = routers.tags.append("v1")
+    routers.include_router(router)
